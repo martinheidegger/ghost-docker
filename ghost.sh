4ccca2ab5d7a1b0baff70b90d1a895562ec4aa5a
Martin Heidegger <martin.heidegger@gmail.com>
2015-04-01 13:40:05 +0900

    Fixed pm2 startups

--- ghost.sh
+++ ghost.sh
@@ -3,7 +3,8 @@ echo "*:*:*:*:$POSTGRES_ENV_POSTGRES_PASSWORD" > ~/.pgpass
 
 chmod 600 ~/.pgpass
 
-pm2 start . --name "ghost"
-pm2 start /cron --name "cron"
+pm2 start index.js --name "ghost"
+cd /cron
+pm2 start index.js --name "cron"
 
 pm2 logs
